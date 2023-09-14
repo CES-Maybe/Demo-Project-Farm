ab574a2e3b23df8c0fa4e0989aa723eebd801898
ces-huy-dang <huy.dang@codeenginestudio.com>
2023-09-14 17:03:18 +0700

    feat: set up farm and product fields
    create fields for farm and product to design farm homepage and product details page
    
    CU-865d32kjk

--- web/modules/custom/farm_page/src/Controller/FarmPageController.php
+++ web/modules/custom/farm_page/src/Controller/FarmPageController.php
@@ -2,7 +2,10 @@
 
 namespace Drupal\farm_page\Controller;
 
+use Drupal\commerce_store\Entity\Store;
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\farm_page\Services\FarmPageService;
+use Drupal\user\Entity\User;
 
 /**
  * Returns responses for Farm Page routes.
@@ -28,16 +31,46 @@ class FarmPageController extends ControllerBase {
    * @return mixed
    *   Return Farmer and Farm information according to current user Id
    */
-  public function myFarm() {
-
-    $uid = \Drupal::currentUser()->id();
-    return [
-      '#theme' => 'farm_homepage',
-      "#uid" => $uid,
-      '#attached' => [
-        'library' => [],
-      ],
-    ];
+  public function myFarm($uid) {
+    $isExist = FALSE;
+    $user = User::load($uid);
+    if ($user == NULL) {
+      $message = '404 user not found';
+      return [
+        '#theme' => 'farm_homepage',
+        '#isExist' => $isExist,
+        "#message" => $message,
+        '#attached' => [
+          'library' => [],
+        ],
+      ];
+    }
+    elseif ($user) {
+      $store_id = FarmPageService::getStoreIdByUserId($uid);
+      if ($store_id == NULL) {
+        $message = '404 store not found';
+        return [
+          '#theme' => 'farm_homepage',
+          '#isExist' => $isExist,
+          "#message" => $message,
+          '#attached' => [
+            'library' => [],
+          ],
+        ];
+      }
+      else {
+        $store = Store::load($store_id);
+        $isExist = TRUE;
+        return [
+          '#theme' => 'farm_homepage',
+          '#isExist' => $isExist,
+          "#store" => $store,
+          '#attached' => [
+            'library' => [],
+          ],
+        ];
+      }
+    }
   }
 
 }
