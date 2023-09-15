af8360423a869eb68b2a44b640e6e2f6346e2c52
nguyenvuces <nguyen.vu@codeenginestudio.com>
2023-09-15 15:24:02 +0700

    feat: add form create customer
    create accounts for customers
    
    CU-865d2cb9u

--- web/modules/custom/maybe_forms/src/Form/CustomerForm.php
+++ web/modules/custom/maybe_forms/src/Form/CustomerForm.php
@@ -6,28 +6,21 @@ use Drupal\Core\Entity\EntityStorageException;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\maybe_forms\Services\AccountService;
-use Drupal\maybe_forms\Services\FarmService;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
- * Defines the FarmForm class.
+ * Defines the CustomerForm class.
  *
  * This class extends Drupal\Core\Form\FormBase to create a custom form for
- * create form farm .
+ * create customer form .
  *
  * Usage:
- * - To access this form, visit the URL path '/farm'.
- * - This form allows administrators add or edit farm.
+ * - To access this form, visit the URL path '/customer'.
+ * - This form allows administrators add customer.
  *
- * @ingroup farm
+ * @ingroup customer
  */
-class FarmForm extends FormBase {
-  /**
-   * Farm Service.
-   *
-   * @var Drupal\maybe_forms\Services\FarmService
-   */
-  protected $farmService;
+class CustomerForm extends FormBase {
   /**
    * Account Service.
    *
@@ -36,16 +29,12 @@ class FarmForm extends FormBase {
   protected $accountService;
 
   /**
-   * Constructs a new instance of the FarmForm class.
+   * Constructs a new instance of the CustomerForm class.
    *
-   * @param \Drupal\your_module\FarmService $farmService
-   *   The FarmService instance responsible
-   *   for managing farm-related operations.
    * @param \Drupal\your_module\AccountService $accountService
    *   The AccountService instance responsible for managing user accounts.
    */
-  public function __construct(FarmService $farmService, AccountService $accountService) {
-    $this->farmService = $farmService;
+  public function __construct(AccountService $accountService) {
     $this->accountService = $accountService;
   }
 
@@ -54,30 +43,29 @@ class FarmForm extends FormBase {
    */
   public static function create(ContainerInterface $container) {
     return new static(
-      $container->get('maybe.farm'),
-      $container->get('maybe.account')
-    );
+          $container->get('maybe.account')
+      );
   }
 
   /**
    * {@inheritdoc}
    *
-   * Gets the unique form ID for the FarmForm.
+   * Gets the unique form ID for the CustomerForm.
    *
    * @return string
    *   The form ID, which is used to identify this form within the system.
    */
   public function getFormId() {
-    return 'maybe_forms_farm';
+    return 'maybe_customer_form';
   }
 
   /**
    * {@inheritdoc}
    *
-   * Builds the FarmForm form.
+   * Builds the CustomerForm form.
    *
    * This method constructs and defines the elements
-   * and structure of the FarmForm
+   * and structure of the CustomerForm
    * form, including fields, buttons, and any other form elements.
    *
    * @param array $form
@@ -88,35 +76,20 @@ class FarmForm extends FormBase {
    *   (Optional) An identifier for the form, if needed for customization.
    */
   public function buildForm(array $form, FormStateInterface $form_state, $id = NULL) {
-    if (isset($id) && is_numeric($id) && ($user = $this->accountService->getUserById($id))) {
-      $form_state->set('id', $id);
-      $address = $user->get('field_address')->getValue();
-      $store = $this->farmService->getFarmByOwnerId($id);
-    }
     $form['address'] = [
       '#type' => 'address',
       '#title' => t('Address'),
-      '#default_value' => !empty($address) ? $address[0] : ['country_code' => 'VN'],
+      '#default_value' => ['country_code' => 'VN'],
     ];
-
     $form['mail'] = [
       '#type' => 'email',
       '#title' => t('Email'),
-      '#default_value' => !empty($user) ? $user->getAccountName() : '',
-      '#required' => TRUE,
-    ];
-
-    $form['name_store'] = [
-      '#type' => 'textfield',
-      '#title' => t('Farm name'),
-      '#default_value' => !empty($store) ? $store->getName() : '',
       '#required' => TRUE,
     ];
     $form['submit'] = [
       '#type' => 'submit',
       '#value' => $this->t('Register'),
     ];
-
     return $form;
   }
 
@@ -125,7 +98,7 @@ class FarmForm extends FormBase {
    */
   public function validateForm(array &$form, FormStateInterface $form_state) {
     $email = $form_state->getValue('mail');
-    if ($this->accountService->checkUserExists($email) && !$form_state->get('id')) {
+    if ($this->accountService->checkUserExists($email)) {
       $form_state->setErrorByName('mail', MAYBE_FORMS_UNIQUE_ERROR_MESSAGE);
     }
   }
@@ -142,29 +115,12 @@ class FarmForm extends FormBase {
         'field_address' => $values['address'],
         'status' => TRUE,
       ];
-      $store = [
-        'address' => $values['address'],
-        'name' => $values['name_store'],
-        'mail' => $values['mail'],
-      ];
-      $user_id = $form_state->get('id');
-      if (!empty($user_id)) {
-
-        if ($this->accountService->updateUserInfo($user_id, $user) && $this->farmService->updateFarmInfo($user_id, $store)) {
-          \Drupal::messenger()->addMessage(MAYBE_FORMS_FARM_UPDATED_MESSAGE);
-        }
-        else {
-          \Drupal::messenger()->addError(MAYBE_FORMS_FARM_UPDATE_ERROR_MESSAGE);
-        }
-      }
-      else {
-        $store['owner_id'] = $this->accountService->createAccount($user, MAYBE_FORMS_FARMER_ROLE);
-        $this->farmService->createStore($store);
-        \Drupal::messenger()->addMessage(MAYBE_FORMS_FARM_CREATED_MESSAGE);
+      if ($this->accountService->createAccount($user, MAYBE_FORMS_CUSTOMER_ROLE)) {
+        \Drupal::messenger()->addMessage(MAYBE_FORMS_CUSTOMER_CREATED_MESSAGE);
       }
     }
     catch (EntityStorageException $e) {
-      \Drupal::messenger()->addError(MAYBE_FORMS_FARM_ERROR_MESSAGE);
+      \Drupal::messenger()->addError(MAYBE_FORMS_CUSTOMER_ERROR_MESSAGE);
     }
   }
 
