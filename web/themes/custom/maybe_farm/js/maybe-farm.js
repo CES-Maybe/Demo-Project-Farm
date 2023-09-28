ffccf450430f76280632abdc63489d9a42f7eef9
ces-huy-dang <huy.dang@codeenginestudio.com>
2023-09-28 11:37:57 +0700

    fix: optimize-js-and-css-code
    replace and js and css code
    
    CU865d55cn6
    
    fix: optimize-js-and-css-code
    
    replace and js and css code
    
    CU-865d55cn6

--- web/themes/custom/maybe_farm/js/maybe-farm.js
+++ web/themes/custom/maybe_farm/js/maybe-farm.js
@@ -7,3 +7,16 @@
     attach: function (context, settings) {}
   };
 })(Drupal);
+
+(function ($) {
+  $(document).ready(function () {
+    // Event listener for thumbnail clicks
+    $('.thumbnail-img').click(function () {
+      // Get the thumbnail's src attribute
+      const newImageSrc = $(this).attr('src');
+
+      // Set the main image's src attribute to the clicked thumbnail's src
+      $('#mainImage').attr('src', newImageSrc);
+    });
+  });
+})(jQuery);
